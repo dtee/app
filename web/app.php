2b01f791e99d9144e8f28d5616cac79624180cac
David Tee <davidkmo@gmail.com>
2011-05-01 05:51:57 -0700

    Fix prod logging name change + app.php will now serve prod env with debugging off.

--- web/app.php
+++ web/app.php
@@ -8,5 +8,5 @@ require_once __DIR__.'/../app/AppKernel.php';
 use Symfony\Component\HttpFoundation\Request;
 
 //$kernel = new AppCache(new AppKernel('prod', false));
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel('prod', false);
 $kernel->handle(Request::createFromGlobals())->send();
