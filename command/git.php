e874d14d83251965dff35e7cb5583feccd0cf81f
David Tee <davidkmo@gmail.com>
2011-05-04 14:54:56 -0700

    One more attempt at sync script, uses rebase instead reset --hard.

---
+++ command/git.php
@@ -0,0 +1,76 @@
+<?php
+$gitRepositories = array(
+	'assetic' => array(
+		'url' => 'git://github.com/dtee/assetic.git',
+		'main_url' => 'git://github.com/kriswallsmith/assetic.git'),
+	'doctrine-common' => array(
+		'url' => 'git://github.com/doctrine/common.git'),
+	'doctrine-orm' => array(
+		'url' => 'git://github.com/doctrine/doctrine2.git',
+		'version' => '2.0.4'),
+	'doctrine-mongodb-odm' => array(
+		'url' => 'git@github.com:dtee/mongodb-odm.git',
+		'main_url' => 'git://github.com/doctrine/mongodb-odm.git'),
+	'doctrine-mongodb' => array(
+		'url' => 'git@github.com:dtee/mongodb-odm.git',
+		'main_url' => 'git://github.com/doctrine/mongodb.git'),
+	'doctrine-dbal' => array(
+		'url' => 'git://github.com/doctrine/dbal.git',
+		'version' => '2.0.4'),
+	'doctrine-migrations' => array(
+		'url' => 'git://github.com/doctrine/migrations.git'),
+	'doctrine-extension' => array(
+		'url' => 'git://github.com/l3pp4rd/DoctrineExtensions.git'),
+	'doctrine-fixtures' => array(
+		'url' => 'https://github.com/doctrine/data-fixtures.git'),
+	'monolog' => array(
+		'url' => 'git://github.com/Seldaek/monolog.git'),
+	'swiftmailer' => array(
+		'url' => 'git://github.com/swiftmailer/swiftmailer.git',
+		'version' => 'origin/4.1'),
+	'twig' => array(
+		'url' => 'git://github.com/fabpot/Twig.git'),
+	'facebook' => array(
+		'url' => 'git://github.com/facebook/php-sdk.git'),
+	'lessc' => array(
+		'url' => 'git://github.com/leafo/lessphp.git'),
+	'symfony' => array(
+		'url' => 'git@github.com:dtee/symfony.git',
+		'main_url' => 'git://github.com/symfony/symfony.git'),
+
+/*	// Node.js
+	'node' => array(
+		'url' => 'https://github.com/joyent/node.git',
+		'path' => 'vendor/node/node'),
+
+	'npm' => array(
+		'url' => 'http://github.com/isaacs/npm.git',
+		'path' => 'vendor/node/npm'), */
+
+	// Bundles
+	'frameworkextra' => array(
+		'url' => 'git://github.com/sensio/SensioFrameworkExtraBundle.git',
+		'path' => 'vendor/bundles/Sensio/Bundle/FrameworkExtraBundle'),
+	'doctrine-mongodb-bundle' => array(
+		'url' => 'git://github.com/symfony/DoctrineMongoDBBundle.git',
+		'path' => 'vendor/bundles/Symfony/Bundle/DoctrineMongoDBBundle'),
+	'jms-security-extra-bundle' => array(
+		'url' => 'git://github.com/schmittjoh/SecurityExtraBundle.git',
+		'path' => 'vendor/bundles/JMS/SecurityExtraBundle'),
+	'DoctrineFixturesBundle' => array(
+		'url' => 'https://github.com/symfony/DoctrineFixturesBundle.git',
+		'path' => 'vendor/bundles/Symfony/Bundle/DoctrineFixturesBundle'),
+
+	// My bundles
+	'asset' => array(
+		'url' => 'git@github.com:dtee/asset.git',
+		'path' => 'src/Odl/AssetBundle'),
+	'shadow' => array(
+		'url' => 'git@github.com:dtee/ShadowBundle.git',
+		'path' => 'src/Odl/ShadowBundle'),
+
+	// css and javascript
+/*	'blueprint-less' => array(
+		'url' => 'git://github.com/sensio/SensioFrameworkExtraBundle.git',
+		'path' => '_css/blueprint-less'), */
+);
--- sync.php
+++ sync.php
@@ -1,80 +1,7 @@
 #!/usr/bin/env php
 
 <?php
-$gitRepositories = array(
-	'assetic' => array(
-		'url' => 'git://github.com/dtee/assetic.git',
-		'main_url' => 'git://github.com/kriswallsmith/assetic.git'),
-	'doctrine-common' => array(
-		'url' => 'git://github.com/doctrine/common.git'),
-	'doctrine-orm' => array(
-		'url' => 'git://github.com/doctrine/doctrine2.git',
-		'version' => '2.0.4'),
-	'doctrine-mongodb-odm' => array(
-		'url' => 'git://github.com/doctrine/mongodb-odm.git'),
-	'doctrine-mongodb' => array(
-		'url' => 'git@github.com:dtee/mongodb-odm.git',
-		'main_url' => 'git://github.com/doctrine/mongodb.git'),
-	'doctrine-dbal' => array(
-		'url' => 'git://github.com/doctrine/dbal.git',
-		'version' => '2.0.4'),
-	'doctrine-migrations' => array(
-		'url' => 'git://github.com/doctrine/migrations.git'),
-	'doctrine-extension' => array(
-		'url' => 'git://github.com/l3pp4rd/DoctrineExtensions.git'),
-	'doctrine-fixtures' => array(
-		'url' => 'https://github.com/doctrine/data-fixtures.git'),
-	'monolog' => array(
-		'url' => 'git://github.com/Seldaek/monolog.git'),
-	'swiftmailer' => array(
-		'url' => 'git://github.com/swiftmailer/swiftmailer.git',
-		'version' => 'origin/4.1'),
-	'twig' => array(
-		'url' => 'git://github.com/fabpot/Twig.git'),
-	'facebook' => array(
-		'url' => 'git://github.com/facebook/php-sdk.git'),
-	'lessc' => array(
-		'url' => 'git://github.com/leafo/lessphp.git'),
-	'symfony' => array(
-		'url' => 'git@github.com:dtee/symfony.git',
-		'main_url' => 'git://github.com/symfony/symfony.git'),
-
-/*	// Node.js
-	'node' => array(
-		'url' => 'https://github.com/joyent/node.git',
-		'path' => 'vendor/node/node'),
-
-	'npm' => array(
-		'url' => 'http://github.com/isaacs/npm.git',
-		'path' => 'vendor/node/npm'), */
-
-	// Bundles
-	'frameworkextra' => array(
-		'url' => 'git://github.com/sensio/SensioFrameworkExtraBundle.git',
-		'path' => 'vendor/bundles/Sensio/Bundle/FrameworkExtraBundle'),
-	'doctrine-mongodb-bundle' => array(
-		'url' => 'git://github.com/symfony/DoctrineMongoDBBundle.git',
-		'path' => 'vendor/bundles/Symfony/Bundle/DoctrineMongoDBBundle'),
-	'jms-security-extra-bundle' => array(
-		'url' => 'git://github.com/schmittjoh/SecurityExtraBundle.git',
-		'path' => 'vendor/bundles/JMS/SecurityExtraBundle'),
-	'DoctrineFixturesBundle' => array(
-		'url' => 'https://github.com/symfony/DoctrineFixturesBundle.git',
-		'path' => 'vendor/bundles/Symfony/Bundle/DoctrineFixturesBundle'),
-
-	// My bundles
-	'asset' => array(
-		'url' => 'git@github.com:dtee/asset.git',
-		'path' => 'src/Odl/AssetBundle'),
-	'shadow' => array(
-		'url' => 'git@github.com:dtee/ShadowBundle.git',
-		'path' => 'src/Odl/ShadowBundle'),
-
-	// css and javascript
-/*	'blueprint-less' => array(
-		'url' => 'git://github.com/sensio/SensioFrameworkExtraBundle.git',
-		'path' => '_css/blueprint-less'), */
-);
+require_once(__DIR__ . '/command/git.php');
 
 $yumInstall = array(
 	'open-ssl-devel', 	// Node.js
@@ -88,7 +15,7 @@ function print_shell_exec($cmd) {
 $installPath =  __DIR__ . '/';
 foreach ($gitRepositories as $key => $info)
 {
-	$version = 'origin/HEAD';
+	$version = 'HEAD';
 	if (isset($info['version']))
 		$version = $info['version'];
 
@@ -101,15 +28,16 @@ foreach ($gitRepositories as $key => $info)
 		$directoryPath = $installPath . 'vendor/' . $key;
 	}
 
+	echo "Target: " . $directoryPath . "\n";
 	if (is_dir($directoryPath))
 	{
+		chdir($directoryPath);
 		// Do we upgrade??
-		echo "Target: {$key}, " . $directoryPath . " exists\n";
+		echo "Target exists...\n";
 		//$command = "git pull";
 		//echo print_shell_exec($command);
 
 		// Make sure the target url is still the same.
-		chdir($directoryPath);
 		echo "path: " . getcwd() . "\n";
 		echo print_shell_exec("git remote rm origin");
 		echo print_shell_exec("git remote rm upstream");
@@ -118,22 +46,25 @@ foreach ($gitRepositories as $key => $info)
 		if (isset($info['main_url']))
 		{
 			echo print_shell_exec("git remote add upstream {$info['main_url']}");
+			echo print_shell_exec("git fetch upstream");
 		}
-		echo print_shell_exec("git pull origin");
+		else
+		{
+			echo print_shell_exec("git fetch origin");
+			echo print_shell_exec("git pull --rebase origin master");
+		}
+
+		//echo print_shell_exec("git reset --hard {$version}");
 	}
 	else
 	{
-		echo "Target: " . $directoryPath . "\n";
-		$clone = "git clone {$info['url']} {$directoryPath}";
-		echo $clone . "\n";
-		echo print_shell_exec($clone) . "\n";
+		echo print_shell_exec("git clone {$info['url']} {$directoryPath}");
 
-		if (isset($info['version']))
+		if ($version)
 		{
+			chdir($directoryPath);
 			echo print_shell_exec("cd {$directoryPath}");
-			$command = "git reset --hard {$info['version']}";
-			echo $command;
-			echo print_shell_exec($command);
+			echo print_shell_exec("git reset --hard {$version}");
 		}
 	}
 
