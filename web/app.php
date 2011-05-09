aef934b4872b734ac2414a7316aea8900319efda
David Tee <davidkmo@gmail.com>
2011-05-09 11:09:36 -0700

    On dev, do not use bootstrap.

--- web/app.php
+++ web/app.php
@@ -2,11 +2,8 @@
 
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/bootstrap_cache.php.cache';
-//require_once __DIR__.'/../app/AppCache.php';
-
 use Symfony\Component\HttpFoundation\Request;
 
-//$kernel = new AppCache(new AppKernel('prod', false));
 $kernel = new AppKernel('prod', false);
+$kernel->loadClassCache('classes', '.php.cache');
 $kernel->handle(Request::createFromGlobals())->send();
--- web/app_dev.php
+++ web/app_dev.php
@@ -2,10 +2,10 @@
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
-require_once __DIR__.'/../app/bootstrap.php.cache';
+require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+require_once __DIR__.'/../app/autoload.php';
 require_once __DIR__.'/../app/AppKernel.php';
 
 use Symfony\Component\HttpFoundation\Request;
-
 $kernel = new AppKernel('dev', true);
 $kernel->handle(Request::createFromGlobals())->send();
