a813a6e0b3a9261fc32ea47aac67cc35cb9748d2
David Tee <davidkmo@gmail.com>
2011-06-01 19:43:55 -0700

    Added new bundles.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -42,6 +42,7 @@ class AppKernel extends Kernel
             new Odl\AuthBundle\OdlAuthBundle(),
             new Odl\ShadowBundle\ShadowBundle(),
             new Odl\PlacesBundle\OdlPlacesBundle(),
+            new Odl\MarketplaceBundle\OdlMarketplaceBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
